b9b59897bdaa1f9839bf925b18823f8aead0f157
Miguel Rios <miguel@adianta.com.br>
2020-05-21 23:10:49 -0300

    debugger config

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
+// eslint-disable-next-line no-console
 app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
