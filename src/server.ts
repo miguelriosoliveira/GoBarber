d325de07b16c08a0f412056c8e69cc723c76f536
Miguel Rios <miguel@adianta.com.br>
2020-05-21 22:57:11 -0300

    prettier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(routes);
 
-app.listen(3333, () => console.log("🚀 Server started on port 3333!"));
+app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
