fd1d48b17b33775d13d8b99362249a35cbb88668
Miguel Rios <miguel@adianta.com.br>
2020-05-24 17:05:06 -0300

    avatar upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
 	try {
@@ -18,5 +22,14 @@ usersRouter.post('/', async (request, response) => {
 		return response.status(400).json({ error: error.message });
 	}
 });
+
+usersRouter.patch(
+	'/avatar',
+	ensureAuthenticated,
+	upload.single('avatar'),
+	async (request, response) => {
+		return response.json({ ok: true });
+	},
+);
 
 export default usersRouter;
