bb59bc0a2d6534d6ee6eac1dbb012b946b1024fa
Miguel Rios <miguel@adianta.com.br>
2020-05-23 23:04:41 -0300

    typeorm configs

---
+++ src/database/migrations/1590275856053-CreateAppointments.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1590275856053 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'appointments',
+				columns: [
+					{
+						name: 'id',
+						type: 'uuid',
+						isPrimary: true,
+						generationStrategy: 'uuid',
+					},
+					{
+						name: 'provider',
+						type: 'varchar',
+					},
+					{
+						name: 'date',
+						type: 'timestamp with time zone',
+					},
+				],
+			}),
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('appointments');
+	}
+}
