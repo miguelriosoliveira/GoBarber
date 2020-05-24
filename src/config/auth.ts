7d0c47b52799c6e48a94ab764a428750b421713c
Miguel Rios <miguel@adianta.com.br>
2020-05-24 03:05:22 -0300

    authentication middleware

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: '7b795927c3bd53749245de27131bebd2',
+		expiresIn: '1d',
+	},
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,8 @@
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -8,8 +10,13 @@ interface Request {
 	password: string;
 }
 
+interface Response {
+	user: User;
+	token: string;
+}
+
 class AuthenticateUserService {
-	public async execute({ email, password }: Request): Promise<User> {
+	public async execute({ email, password }: Request): Promise<Response> {
 		const usersRepository = getRepository(User);
 
 		const user = await usersRepository.findOne({ where: { email } });
@@ -22,7 +29,10 @@ class AuthenticateUserService {
 			throw new Error('Incorrect email/password combination');
 		}
 
-		return user;
+		const { secret, expiresIn } = authConfig.jwt;
+		const token = sign({}, secret, { subject: user.id, expiresIn });
+
+		return { user, token };
 	}
 }
 
