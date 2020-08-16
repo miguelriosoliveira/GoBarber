99d79086d2322ef228fb941c5d79e97994f9e91a
Miguel Rios <miguel@adianta.com.br>
2020-08-16 20:21:56 -0300

    added business rules to appointment creation

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -21,6 +21,22 @@ class CreateAppointmentService {
 
 	public async execute({ provider_id, user_id, date }: IRequest): Promise<Appointment> {
 		const appointmentDate = startOfHour(date);
+
+		if (provider_id === user_id) {
+			throw new AppError("You can't create an appointment for yourself");
+		}
+
+		if (isBefore(appointmentDate, Date.now())) {
+			throw new AppError("You can't create an appointment on a past date");
+		}
+
+		const startHour = 8;
+		const endHour = 17;
+		const appointmentHour = appointmentDate.getHours();
+
+		if (appointmentHour < startHour || appointmentHour > endHour) {
+			throw new AppError('You can only create a appointments between 8am and 5pm');
+		}
 
 		const appointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
 		if (appointmentInSameDate) {
