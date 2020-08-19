391d1e9c09a7e8430b62a3f686e231c4ed1f425d
Miguel Rios <miguel@adianta.com.br>
2020-08-18 22:18:58 -0300

    Creating notifications when creating an appointment

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,8 @@ import { container } from 'tsyringe';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -18,3 +20,8 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
 
 container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository);
+
+container.registerSingleton<INotificationsRepository>(
+	'NotificationsRepository',
+	NotificationsRepository,
+);
